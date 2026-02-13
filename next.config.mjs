895b777355381187226b9a17e39263aaddc09ef7
yoomeeshin <yoomeeshinn@gmail.com>
2026-02-13 08:01:34 -0500

    feat: configure build tools and TypeScript

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // output: 'export',
+    /* config options here */
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+};
+
+export default nextConfig;
